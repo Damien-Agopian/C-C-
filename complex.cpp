3a801f0cecfd3a66dffb822e0296c98a5a9ec920
Damien <damien.agopian@insa-lyon.fr>
2016-12-08 18:40:29 +0100

    Inverse .h and .cpp

--- complex.cpp
+++ complex.cpp
@@ -1,17 +1,1 @@
-#include <stdio.h>
-
-class complex{
-	private :
-		int reelle;
-		int imaginaire;
-	public : 
-		//Getters
-		int get_reelle;
-		int get_imaginaire;
-		//Setters
-		int set_reelle;
-		int set_imaginaire;
-		//Constructor
-		complex(); //default constructor
-		comeplex(int reelle, int imaginaire) 
-};
+#include "complex.h"
--- complex.h
+++ complex.h
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+class complex{
+	private :
+		int reelle;
+		int imaginaire;
+	public : 
+		//Getters
+		int get_reelle;
+		int get_imaginaire;
+		//Setters
+		int set_reelle;
+		int set_imaginaire;
+		//Constructor
+		complex(); //default constructor
+		comeplex(int reelle, int imaginaire) 
+};
 
