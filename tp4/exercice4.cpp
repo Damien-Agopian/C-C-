5175211745e6f70d15792f1b741f7eb3c9c8bdde
Damien <damien.agopian@insa-lyon.fr>
2016-12-24 15:19:27 +0100

    Maj

--- tp4/exercice4.cpp
+++ tp4/exercice4.cpp
@@ -1,20 +1,41 @@
-#include <stdio.h>
+#include <iostream>
 #include <string>
+#include <stdio.h>
 
+using namespace std;
 
 int chance(char date_de_naissance[]){
 	int nombre_chance = date_de_naissance[0]-'0';
+	
+	cout << nombre_chance << endl;
+
 	nombre_chance += date_de_naissance[1]-'0';
+	
+	cout << nombre_chance << endl;
+
+	int taille_mot = 0;
+	int c = '1';
+	while(c != '\0'){  //On determine la taille de la chaine
+		c = date_de_naissance[taille_mot];
+		taille_mot++;
+	}
+	for(int i = taille_mot-1 ; i> taille_mot-6 ; i--){
+		cout << date_de_naissance[i] << "  " << nombre_chance << endl;
+		nombre_chance += date_de_naissance[i];
+	}
+
+
 	//for(i=1; i<=4 ; ++i){
 		//nombre_chance += date_de_naissance[sizeof(date_de_naissance)/sizeof(date_de_naissance[0])-i];
 	//}
-	return nombre_chance
+	return nombre_chance;
 }	
 
 
 
 int main(){
 	int r = chance("31-Decembre-1996");
-	printf("%d\n",r);
+	cout << r << endl;
+	//printf("%d\n",r);
 }
 	
--- tp4/exercice5.cpp
+++ tp4/exercice5.cpp
@@ -2,8 +2,18 @@
 
 using namespace std;
 bool palindrome(char mot[]){
-	int j=sizeof(mot);
-	for (int i=0; i< sizeof(mot); i++){
+	int taille_mot = 0;
+	char compteur = '1';
+	while(compteur != '\0'){  //On determine la taille de la chaine
+		compteur = mot[taille_mot];
+		taille_mot++;
+	}
+	cout <<"la taille du mot est: " << taille_mot-1 <<endl;
+	int j = taille_mot -2;
+	for (int i = 0; i < taille_mot; i++){
+		//On parcours la chaine de chaques coté
+
+		//(debugage) cout << "i = " << i << " et j = "<< j << endl;
 		if(mot[i] != mot[j]){
 			return false;
 		}
@@ -14,10 +24,18 @@ bool palindrome(char mot[]){
 	return true ;
 }
 
-bool main(){
+int main(){
 	char mot[255];
 	cout << "Entrez le mot a tester : " << endl;
-	cin >> palindrome(mot);
+	
+	cin >> mot;
+	if(palindrome(mot) ==true){
+		cout << mot << " est bien un palindrome" << endl;
+
+	}
+	else{
+		cout << mot << " n'est pas un palindrome" << endl;
+	}
 	return 0;
 
 }
